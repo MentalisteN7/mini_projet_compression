38def7e0ece5f98ef911a0321e613d1e6bc4fedc
Valentin <valentin.le_tallec@hotmail.com>
2021-01-19 17:05:37 +0100

    calcule des Q

--- compress_obj.py
+++ compress_obj.py
@@ -1,17 +1,18 @@
+from typing import Tuple
 from obj_loader import ObjLoader
+import numpy as np
 
 obj = 'bunny_origin.obj'
 
 lapin = ObjLoader(obj)
-print(lapin.vertices[lapin.faces[0][0]])
 # 1.  Compute the Q matrices for all the initial vertices.
 
-def Kp(a, b, c, d):
+def getKp(a, b, c, d):
     return [[a*a, a*b, a*c, a*d],
             [a*b, b*b, b*c, b*d],
             [a*d, b*d, c*d, d*d]]
 
-def planeEquation(v1, v2, v3):
+def getPlaneEquation(v1, v2, v3):
     a = (v2[1] - v1[1]) * (v3[2] - v1[2]) - (v3[1] - v1[1])*(v2[2] - v1[2])
     b = (v2[2] - v1[2]) * (v3[0] - v1[0]) - (v3[2] - v1[2])*(v2[0] - v1[0])
     c = (v2[0] - v1[0]) * (v3[1] - v1[1]) - (v3[0] - v1[0])*(v2[1] - v1[1])
@@ -26,14 +27,31 @@ def getListKp(obj: ObjLoader):
         v1 = obj.vertices[face[0]-1]
         v2 = obj.vertices[face[1]-1]
         v3 = obj.vertices[face[2]-1]
-        a, b, c, d = planeEquation(v1, v2, v3)
-        listKp += [Kp(a, b, c, d)]
+        a, b, c, d = getPlaneEquation(v1, v2, v3)
+        listKp += [getKp(a, b, c, d)]
     return listKp
+
+def getListQ(obj: ObjLoader, listK):
+    listQ = []
+    for vertex_index in range(1,len(obj.vertices) + 1):
+        Q = np.matrix([[0., 0., 0., 0.],
+                       [0., 0., 0., 0.],
+                       [0., 0., 0., 0.]])
+
+        for face_index in range(len(obj.faces)):
+            if vertex_index in obj.faces[face_index]:
+                Q += np.matrix(listKp[face_index])
+        # print(Q)
+        # print('\n\n')
+        listQ += [Q]
+    return listQ
 
 # a, b, c, d = planeEquation(lapin.vertices[lapin.faces[0][0]], lapin.vertices[lapin.faces[0][1]], lapin.vertices[lapin.faces[0][2]])
 # kp = Kp(a, b, c, d)
 listKp = getListKp(lapin)
-print(listKp[0])
+listQ = getListQ(lapin, listKp)
+print(listQ[0])
+# print(listKp[1])
 # 2.  Select all valid pairs.
 # 3.  Compute the optimal contraction target Nv for each valid pair.v1;v2/. 
 # The errorNvT.Q1CQ2/Nv of this target vertex becomes the cost of contracting that pair.
