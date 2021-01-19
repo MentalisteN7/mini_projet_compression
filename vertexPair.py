f518de2b6fbf1e5791e14f9692fcd20203ec74b8
rcontrai <romain.contrain@free.fr>
2021-01-19 17:27:23 +0100

    commencé classe vertexpair

---
+++ vertexPair.py
@@ -0,0 +1,18 @@
+# from dataclasses import dataclass, field
+# from typing import Any
+import numpy as np
+# 
+# @dataclass(order=True)
+# class PrioritizedItem:
+#     priority: float
+#     item: Any=field(compare=False)
+
+class vertexPair:
+    vindices : tuple
+    cost : float
+    v_bar : numpy.ndarray
+    
+    def __init__(self, indices):
+        self.vindices = indices
+        
+
